6e746eee346f79bcdd3bfac6a85c8a3d125ca80b
geeko <george.kvrivi@gmail.com>
2023-07-20 15:56:54 +0400

    refactor: return quote object with its author

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -27,7 +27,7 @@ class QuoteController extends Controller
 			'image'    => $request->file('image')->store($directoryPath),
 			'user_id'  => auth()->user()->id,
 		]);
-		return response()->json($quote->load(['likes','comments']), 201);
+		return response()->json($quote->load(['likes','comments','author']), 201);
 	}
 
 	public function destroy(int $quoteId): JsonResponse
